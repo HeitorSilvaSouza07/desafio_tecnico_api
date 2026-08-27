89b1de7ae1b61e39539c4ba93600a275a210dd12
HeitorSilvaSouza77 <euclidesde@gmail.com>
2026-08-26 22:35:01 -0300

    fix

--- src/controller/adminController.ts
+++
@@ -1,9 +0,0 @@
-import type { Request, Response } from 'express';
-
-export class AdminController{
-
-    static async teste(req: Request, res: Response){
-        return res.status(200).json({'msg': 'Banco trabalhando'});
-    }
-
-}
--- src/routes/admin.ts
+++
@@ -1,8 +0,0 @@
-import Router from 'express';
-import { AdminController } from '../controller/adminController.js';
-
-const router = Router();
-
-router.get('/', AdminController.teste);
-
-export default router;
